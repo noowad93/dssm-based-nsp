d8ce71dc210ca3a7ce5a98d13595c111998c1483
noowad93 <da0968@naver.com>
2020-02-03 21:11:06 +0900

    build: 레포 초기 설정 변경

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="python-37-template-repository",
+    name="dssm-based-nsp",
     version="0.0.1",
-    description="python template repository",
+    description="Deep Structured Semantic Models-based Next Sentence Prediction",
     install_requires=[],
-    url="https://github.com/scatterlab/python-37-template-repository.git",
+    url="https://github.com/noowad93/dssm-based-nsp.git",
     author="ScatterLab",
     author_email="developers@scatterlab.co.kr",
     packages=find_packages(exclude=["tests"]),
